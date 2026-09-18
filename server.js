8077b56b68ff35629d99af58b023870de6e84e9c
Nathan Heitzman <nathanheitzman200@gmail.com>
2026-09-17 21:15:05 -0500

    started on styling

--- server.js
+++ server.js
@@ -5,19 +5,24 @@ import path from 'path';
 const port = 8080
 const app = express()
 
+//find absolute paths
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
+//pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(dirname, 'pages', 'home.html'));
 })
 
+//styles
 app.get('/styles/global/colors.css', (req, res) => {
     res.sendFile(path.join(dirname, 'styles', 'global', 'colors.css'));
 })
-
 app.get('/styles/home.css', (req,res) => {
     res.sendFile(path.join(dirname, 'styles', 'home.css'));
+})
+app.get('/styles/rules.css', (req,res) => {
+    res.sendFile(path.join(dirname, 'styles', 'global', 'rules.css'));
 })
 
 app.listen(8080, () => {
