8eb63fb3018021b4a3f6ccea36b8dbae3096a8b4
Nathan Heitzman <nathanheitzman200@gmail.com>
2026-09-17 20:24:53 -0500

    connected style page

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(dirname, 'pages', 'home.html'));
 })
 
+app.get('/styles/global/colors.css', (req, res) => {
+    res.sendFile(path.join(dirname, 'styles', 'global', 'colors.css'));
+})
+
+app.get('/styles/home.css', (req,res) => {
+    res.sendFile(path.join(dirname, 'styles', 'home.css'));
+})
+
 app.listen(8080, () => {
     console.log('http://localhost:%s', port)
 })
